e25d0a19d4addf00a1e2d93d7d962304ddd619f4
hobbescodes <jakob.hauble@gmail.com>
2022-06-30 19:46:25 -0500

    add deploy governance token script + configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,24 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: "0.8.8",
-};
+import "hardhat-deploy"
+import "@nomiclabs/hardhat-ethers"
+import "@typechain/hardhat"
+import { HardhatUserConfig } from "hardhat/config"
+
+const config: HardhatUserConfig = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+    },
+    solidity: "0.8.8",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+}
+
+export default config
