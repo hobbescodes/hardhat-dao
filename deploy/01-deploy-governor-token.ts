066a67d5fc7b2b359db07f29859a66a7d8f33605
hobbescodes <jakob.hauble@gmail.com>
2022-06-30 20:05:44 -0500

    add deploy governance token script + configs

---
+++ deploy/01-deploy-governor-token.ts
@@ -0,0 +1,53 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { networkConfig, developmentChains } from "../hardhat-helper-config";
+import verify from "../utils/verify";
+import { ethers } from "hardhat";
+
+const deployGovernanceToken: DeployFunction = async (
+  hre: HardhatRuntimeEnvironment
+) => {
+  const { getNamedAccounts, deployments, network } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const MAX_SUPPLY = ethers.utils.parseUnits("1000000");
+
+  const blockConfirmations = developmentChains.includes(network.name)
+    ? 1
+    : networkConfig[network.name]["blockConfirmations"];
+
+  log("Deploying Governance Token....");
+  const governanceToken = await deploy("GovernanceToken", {
+    from: deployer,
+    args: [MAX_SUPPLY],
+    log: true,
+    waitConfirmations: blockConfirmations,
+  });
+  log(`Deployed Governance Token to ${governanceToken.address}`);
+
+  await delegate(governanceToken.address, deployer);
+
+  if (!developmentChains.includes(network.name)) {
+    await verify(governanceToken.address, [ethers.utils.parseUnits("1000000")]);
+  }
+};
+
+const delegate = async (
+  governanceTokenAddress: string,
+  delegatedAccount: string
+) => {
+  const governanceToken = await ethers.getContractAt(
+    "GovernanceToken",
+    governanceTokenAddress
+  );
+  // delegates the voting power of the delegatedAccount
+  const tx = await governanceToken.delegate(delegatedAccount);
+  await tx.wait(1);
+  // numCheckpoints essentially establishes how much voting power delegatedAccount has
+  console.log(
+    `Checkpoints ${await governanceToken.numCheckpoints(delegatedAccount)}`
+  );
+};
+
+export default deployGovernanceToken;
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,28 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: "0.8.8",
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-deploy";
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true,
+    },
+    localhost: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true,
+    },
+  },
+  solidity: {
+    version: "0.8.8",
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+    },
+  },
 };
+
+export default config;
